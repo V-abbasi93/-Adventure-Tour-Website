98e9a2889068eb9da296ff501671843ac9a76d11
Vajihe <v.abbasi9372@gmail.com>
2022-12-01 00:34:37 +0100

    Create Blogs Section

--- js/script.js
+++ js/script.js
@@ -96,3 +96,24 @@ var swiper = new Swiper(".review-slider", {
         },
       },
 });
+
+var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+          0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+});
